9066ef6023730925ce61c297d4229dff1c24bf69
tanyaestici <tatiana.aioa@gmail.com>
2023-04-20 01:54:17 +0500

    Refactoring
    Introduces a separate Telegram client.   Adds Telegram response status handling.

--- main.py
+++ main.py
@@ -3,9 +3,6 @@ import httpx
 from time import sleep
 from random import choice
 
-API_URL = "https://api.telegram.org"
-API_TOKEN = os.environ.get("API_TOKEN", None)
-API_URL_FULL = f"{API_URL}/bot{API_TOKEN}"
 
 REPLIES = [
     "Please, abstain from sending voices",
@@ -17,54 +14,70 @@ REPLIES = [
 ]
 
 
-def handle_message(message):
-    chat_id = message["chat"]["id"]
-    reply_to_id = message["message_id"]
+class TelegramClient:
+    API_URL = "https://api.telegram.org"
 
-    httpx.post(
-        f"{API_URL_FULL}/sendMessage",
-        data={
-            "chat_id": chat_id,
-            "text": choice(REPLIES),
-            "reply_to_message_id": reply_to_id,
-        },
-    )
+    def __init__(self, token):
+        self.token = token
+        self.api_url_full = f"{self.API_URL}/bot{self.token}"
+
+    def get_messages(self, offset):
+        r = httpx.post(
+            f"{self.api_url_full}/getUpdates",
+            data={"allowed_updates": ["message"], "offset": offset},
+        )
+        response = r.json()
+
+        if not response["ok"]:
+            print(response["error_code"], response["description"])
+            return []
+        return response["result"]
+
+    def send_messages(self, chat_id, text, reply_to_message_id=None):
+        httpx.post(
+            f"{self.api_url_full}/sendMessage",
+            data={
+                "chat_id": chat_id,
+                "text": choice(REPLIES),
+                "reply_to_message_id": reply_to_message_id,
+            },
+        )
 
 
-def request(last_update_id):
+def request(client, last_update_id):
     offset = 0
 
     if last_update_id:
         offset = last_update_id + 1
 
-    r = httpx.get(
-        f"{API_URL_FULL}/getUpdates",
-        params={"allowed_updates": ["message"], "offset": offset},
-    )
+    messages = client.get_messages(offset)
 
-    response = r.json()
-    updates = response["result"]
-
-    for update in updates:
-        last_update_id = update["update_id"]
-        message = update.get("message", None)
+    for message in messages:
+        last_update_id = message["update_id"]
+        message = message.get("message", None)
 
         if message and "voice" in message:
-            handle_message(message)
+            chat_id = message["chat"]["id"]
+            reply_to_message_id = message["message_id"]
+
+            client.send_messages(chat_id, choice(REPLIES), reply_to_message_id)
 
     return last_update_id
 
 
 def main():
-    if not API_TOKEN:
+    token = os.environ.get("API_TOKEN", None)
+
+    if not token:
         print("error: Telegram API token (API_TOKEN) is required")
         exit(1)
 
     last_update_id = 0
+    client = TelegramClient(token)
 
     while True:
         try:
-            last_update_id = request(last_update_id)
+            last_update_id = request(client, last_update_id)
         except httpx.RequestError:
             pass
         sleep(2)
